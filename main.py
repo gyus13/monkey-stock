d508f259abefaec858d4ffc25d27ef8c9ebe3796
SeSAC-Cloud-CTW <slowlight50@gmail.com>
2022-04-23 19:33:53 +0900

    edit logging

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_page(pageNumber, sise):
         sise.append([ele for ele in cols if ele])
 
 def crawl():
-    print("crawling start at: " + time.localtime().strftime())
+    print("crawling start at: " + time.strftime("%Y.%m.%d %H:%M:%S", time.localtime()))
     start = time.time()
     df = []
     sise = []
